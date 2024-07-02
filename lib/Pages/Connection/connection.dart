99ae96d6100ad0499ae747117cd263264f9868b5
Smokyhead <saidane.sirine2001@gmail.com>
2024-07-02 23:08:44 +0200

    0.0.2

--- lib/Pages/Connection/connection.dart
+++ lib/Pages/Connection/connection.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: file_names
 import 'package:flutter/material.dart';
+import 'package:iconly/iconly.dart';
+import 'package:managit/pages/Connection/password_recovery.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -8,8 +11,126 @@ class Login extends StatefulWidget {
 }
 
 class LoginState extends State<Login> {
+  final myController1 = TextEditingController();
+  final myController2 = TextEditingController();
+  bool hidePassword = true;
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Center(
+          child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 30,
+            ),
+            const Text(
+              "Connectez-vous",
+              style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.indigo,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            const SizedBox(height: 60),
+            TextFieldContainer(
+              child: TextFormField(
+                obscureText: hidePassword,
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                    hintText: "Votre Email",
+                    border: InputBorder.none,
+                    icon: Icon(
+                      IconlyLight.profile,
+                      color: Colors.indigo,
+                    )),
+                controller: myController1,
+              ),
+            ),
+            const SizedBox(height: 5),
+            TextFieldContainer(
+                child: TextFormField(
+              textAlignVertical: TextAlignVertical.center,
+              obscureText: hidePassword,
+              decoration: InputDecoration(
+                icon: const Icon(
+                  IconlyLight.lock,
+                  color: Colors.indigo,
+                ),
+                suffixIcon: IconButton(
+                    icon: Icon(
+                      hidePassword ? IconlyLight.hide : IconlyLight.show,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        hidePassword = !hidePassword;
+                      });
+                    },
+                    color: Colors.indigo),
+                border: InputBorder.none,
+                hintText: "Votre Mot de passe",
+              ),
+              controller: myController2,
+            )),
+            const SizedBox(height: 25),
+            Container(
+              margin: const EdgeInsetsDirectional.only(top: 5),
+              width: size.width * 0.8,
+              height: 60,
+              child: TextButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    elevation: WidgetStateProperty.all(10),
+                    backgroundColor: WidgetStateProperty.all(Colors.indigo),
+                    foregroundColor: WidgetStateProperty.all(Colors.white),
+                    shape: WidgetStateProperty.all(
+                      RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                    ),
+                  ),
+                  child: const Text("Se connecter",
+                      style: TextStyle(fontSize: 20))),
+            ),
+            const SizedBox(height: 10),
+            TextButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return const ResetPassword();
+                  }));
+                },
+                child: const Text(
+                  'Récupérez Mot de passe',
+                  style: TextStyle(color: Colors.indigo),
+                )),
+            const SizedBox(height: 50),
+          ],
+        ),
+      )),
+    );
+  }
+}
+
+class TextFieldContainer extends StatelessWidget {
+  final Widget child;
+  const TextFieldContainer({
+    super.key,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 6),
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+      width: size.width * 0.8,
+      decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 224, 227, 241),
+          borderRadius: BorderRadius.circular(20)),
+      child: child,
+    );
   }
 }
---
+++ lib/Pages/Connection/password_recovery.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+
+class ResetPassword extends StatefulWidget {
+  const ResetPassword({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _ResetPassword();
+}
+
+class _ResetPassword extends State<ResetPassword> {
+  final TextEditingController emailController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(60),
+        child: AppBar(
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(15),
+          )),
+          backgroundColor: Colors.indigo,
+          foregroundColor: Colors.white,
+          title: const Text("Mot de passe"),
+        ),
+      ),
+      backgroundColor: Colors.white,
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/images/Untitled design.jpg'),
+              fit: BoxFit.cover,
+              opacity: 0.5),
+        ),
+        child: SizedBox(
+          height: size.height,
+          width: double.infinity,
+          child: Stack(
+            alignment: Alignment.center,
+            children: <Widget>[
+              SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      margin: const EdgeInsets.only(
+                          right: 20, left: 20, bottom: 100),
+                      child: const Text(
+                        "Récuperer votre mot de passe",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 26,
+                            color: Colors.indigo),
+                      ),
+                    ),
+                    Container(
+                      margin:
+                          const EdgeInsets.only(right: 5, left: 5, bottom: 25),
+                      child: const Text(
+                        "Veuillez saisir votre adresse email",
+                        style: TextStyle(
+                          fontSize: 17,
+                        ),
+                      ),
+                    ),
+                    TextFieldContainer(
+                      child: TextFormField(
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: const InputDecoration(
+                          hintText: "Email",
+                          border: InputBorder.none,
+                        ),
+                        controller: emailController,
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsetsDirectional.only(top: 50),
+                      width: 150,
+                      height: 50,
+                      child: TextButton(
+                        onPressed: () {},
+                        style: ButtonStyle(
+                          elevation: WidgetStateProperty.all(5),
+                          backgroundColor:
+                              WidgetStateProperty.all(Colors.indigo),
+                          foregroundColor:
+                              WidgetStateProperty.all(Colors.white),
+                          shape: WidgetStateProperty.all(
+                            RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20)),
+                          ),
+                        ),
+                        child: const Text("Valider",
+                            style: TextStyle(fontSize: 19)),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 50,
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TextFieldContainer extends StatelessWidget {
+  final Widget child;
+  const TextFieldContainer({
+    super.key,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 6),
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+      width: size.width * 0.8,
+      decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 224, 227, 241),
+          borderRadius: BorderRadius.circular(20)),
+      child: child,
+    );
+  }
+}
