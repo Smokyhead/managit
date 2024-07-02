97b22f904de2b5215d37f80fe6fb725430dcb35b
Smokyhead <saidane.sirine2001@gmail.com>
2024-07-02 19:32:43 +0200

    0.0.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:managit/pages/Connection/Connection.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Manag''IT',
-      home: Scaffold(),
+      home: Login(),
     );
   }
 }
